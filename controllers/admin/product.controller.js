29461c5d06e0a530aada27527798aa92ce0ca878
tranquangminh0110 <bibabibum0110@gmail.com>
2024-03-25 14:02:27 +0700

    get product from db for admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,10 +1,22 @@
+const Product = require("../../models/product.model");
+
 /**
  * @method GET - /admin/products
  *
  */
+module.exports.index = async (req, res) => {
+  try {
+    let queryString = {
+      deleted: false,
+    };
 
-module.exports.index = (req, res) => {
-  res.render("./admin/pages/product/index", {
-    title: "List Products",
-  });
+    const data = await Product.find(queryString);
+
+    res.render("./admin/pages/product/index", {
+      title: "List Products",
+      products: data,
+    });
+  } catch (error) {
+    res.render("./admin/pages/not-found/index");
+  }
 };
