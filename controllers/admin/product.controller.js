29cacca8d0de79562915c8473da5ab33b06e52cd
tranquangminh0110 <bibabibum0110@gmail.com>
2024-03-25 19:08:42 +0700

    update clear filter & search by text

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,29 +1,37 @@
 const Product = require("../../models/product.model");
 const { capitalize } = require("../../utils/helper");
+const { objectTextSearch } = require("../../utils/helper");
+const { objectStatusFilter } = require("../../utils/helper");
+
 /**
  * @method GET - /admin/products
- *
  */
+
 module.exports.index = async (req, res) => {
   try {
     let currentStatus;
+
     let queryString = {
       deleted: false,
     };
 
+    const objectSearch = objectTextSearch(req.query);
+
     if (req.query.status) {
-      if (req.query.status !== "all") {
-        queryString.status = req.query.status;
-        currentStatus = capitalize(req.query.status);
-      }
+      queryString.status = req.query.status;
+      currentStatus = capitalize(req.query.status);
+    }
+
+    if (objectSearch.title) {
+      queryString.title = objectSearch.title;
     }
 
     const data = await Product.find(queryString);
-
     res.render("./admin/pages/product/index", {
       title: "List Products",
       products: data,
       currentStatus: currentStatus || "All",
+      textSearch: objectSearch.textSearch,
     });
   } catch (error) {
     res.render("./admin/pages/not-found/index");
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -6,31 +6,51 @@ const myFiterBtn = $("#filter-btn");
 const listStatus = $$("[active-status]");
 
 if (myFiterBtn) {
-  if (listStatus.length !== 0) {
+  if (listStatus.length > 0) {
     listStatus.forEach((btn) => {
-      let newURL = new URL(window.location.href);
+      let url = new URL(window.location.href);
       btn.addEventListener("click", () => {
         const valueStatus = btn.getAttribute("active-status");
         if (valueStatus) {
-          const currentStatus = btn.getAttribute("active-status");
-          newURL.searchParams.set("status", currentStatus);
+          url.searchParams.set("status", valueStatus);
+        } else {
+          url.searchParams.delete("status");
         }
-        window.location.href = newURL;
+        // Điều hướng web sang một url mới
+        window.location.href = url;
       });
     });
   }
 }
-
 // End Filter
 
 // Clear Filter
 const clearFilterBtn = $("#btn-clear-filter");
 if (clearFilterBtn) {
   clearFilterBtn.addEventListener("click", () => {
-    let newURL = new URL(window.location.href);
-    const filterUrl = newURL.href.split("?");
-    window.location.href = filterUrl[0];
+    let url = new URL(window.location.href);
+    if (url.href.includes("?")) {
+      const filterUrl = url.href.split("?");
+      window.location.href = filterUrl[0];
+    }
   });
 }
-
 // End Clear Filter
+
+// Find by text search
+const formSearch = $("#form-search");
+
+if (formSearch) {
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const valueTextSearch = e.target.elements["keyword"].value;
+    if (valueTextSearch.trim()) {
+      let url = new URL(window.location.href);
+      url.searchParams.set("keyword", valueTextSearch);
+      window.location.href = url;
+    } else {
+      alert("Please input after submit");
+    }
+  });
+}
+// End find by text search
