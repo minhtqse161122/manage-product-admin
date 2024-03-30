9c02227d283812e83348e45daa7c51ab5415d31a
tranquangminh0110 <bibabibum0110@gmail.com>
2024-03-30 15:08:30 +0700

    add timestamp for create method

--- models/product.model.js
+++ models/product.model.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
 
 // Tạo Schema cho Product Model
-const productSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  price: Number,
-  discountPercentage: Number,
-  stock: Number,
-  thumbnail: String,
-  status: String,
-  position: Number,
-  deleted: Boolean,
-  deletedAt: Date,
-});
+const productSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    price: Number,
+    discountPercentage: Number,
+    stock: Number,
+    thumbnail: String,
+    status: String,
+    position: Number,
+    deleted: Boolean,
+    deletedAt: Date,
+  },
+  { timestamps: true }
+);
 
 //Tạo Model từ Product Schema
 const Product = mongoose.model("Product", productSchema, "products");
